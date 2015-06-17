ad161ad14769d2e6c513c6c914fba117c404ac9c
Cihan Erkut <erkut@mpi-cbg.de>
2015-06-17 21:42:42 +0200

    Added query for gene classes and display of individual strains (in hypometabolism experiment), also optimized the code a lot

--- kmdb/app.R
+++ kmdb/app.R
@@ -3,15 +3,22 @@ library(shiny)
 library(ggplot2)
 library(xtable)
 
+# Load data
+expr1 <- read.delim('expr_desiccation.txt', sep = '\t', header = T)
+expr2 <- read.delim('results_desiccation.txt', sep = '\t', header = T)
+expr3 <- read.delim('expr_hypometabolism.txt', sep = '\t', header = T, as.is = T)
+expr4 <- read.delim('results_hypometabolism.txt', sep = '\t', header = T, as.is = T)
+
+expr1$Gene <- as.vector(expr1$Gene)
+expr3$Gene <- as.vector(expr3$Gene)
+expr3$Stage <- factor(expr3$Stage, c('L3', 'dauer'))
+expr3$Strain <- factor(expr3$Strain, c('N2', 'daf-2', 'daf-16', 'daf-2;daf-12'))
+expr4$Strain <- factor(expr4$Strain, c('N2', 'daf-2', 'daf-16', 'daf-2;daf-12'))
+
+geneClass1 <- sort(unique(gsub('-[[:alnum:]]+', '', grep('-[[:alnum:]]+', expr1$Gene, value = T))))
+geneClass2 <- sort(unique(gsub('-[[:alnum:]]+', '', grep('-[[:alnum:]]+', expr3$Gene, value = T))))
+
 # Common definitions
-file.expr1 <- 'expr_desiccation.txt'
-file.expr2 <- 'results_desiccation.txt'
-file.expr3 <- 'expr_hypometabolism.txt'
-file.expr4 <- 'results_hypometabolism.txt'
-
-strains <- c('N2', 'daf-2', 'daf-16', 'daf-2;daf-12')
-stages <- c('L3', 'dauer')
-
 wb.url <- 'http://www.wormbase.org/species/c_elegans/gene/'
 
 common.theme <- theme(legend.position = "bottom", 
@@ -25,51 +32,49 @@ common.theme <- theme(legend.position = "bottom",
 msgPlot <- 'This plot will update as you enter valid gene names!'
 msgTable <- 'This table will update as you enter valid gene names!'
 msgEntry <- 'Type in valid gene names separated by comma (e.g. tps-1, F08H9.4)'
+msgStrain <- 'This condition is not selected for analysis!'
+msgCondition <- 'This plot will update as you select strains!'
 
 # Utility functions
 trim <- function(x) {
   pattern <- '(^[[:space:]]+|[[:space:]]+$)'
   trimmed <- gsub(pattern, '', x)}
 
-expr.subset <- function(expr.df, genelist) {
-  genes.selected <- trim(unlist(strsplit(genelist, ',')))
+expr.subset <- function(expr.df, geneList) {
+  genes.selected <- trim(unlist(strsplit(geneList, ',')))
   expr.selected <- expr.df[expr.df$Gene %in% genes.selected, ]}
 
 link.wb <- function(x) {
   HTML(as.character(a(href = paste(wb.url, x, sep = ''), x, target = '_blank')))}
 
+buildExpr <- function(expr.df, input.query, input.text, input.geneClass, input.conditions = NULL) {
+  if(input.query == 'Genes') {
+    expr.new <- expr.subset(expr.df, input.text)
+  }
+  else {
+    classPattern <- paste('^', input.geneClass, '-', sep = '')
+    expr.new <- expr.subset(expr.df, grep(classPattern, expr.df$Gene, value = T))
+  }
+  if(is.vector(input.conditions)) {
+    expr.new <- expr.new[expr.new$Strain %in% input.conditions,]
+  }
+  return(expr.new)
+}
+
 # Server function
 server <- function(input, output) {
-  # Load data while displaying progress
-  withProgress(message = 'Loading data... ', value = 0, {
-                 
-    expr1 <- read.delim(file.expr1, sep = '\t', header = T)
-    incProgress(0.25)
 
-    expr2 <- read.delim(file.expr2, sep = '\t', header = T)
-    incProgress(0.2)
-    
-    expr3 <- read.delim(file.expr3, sep = '\t', header = T, as.is = T)
-    incProgress(0.25)
-    
-    expr4 <- read.delim(file.expr4, sep = '\t', header = T, as.is = T)
-    incProgress(0.2, message = 'Arranging data...')
-    
-    expr1$Gene <- as.vector(expr1$Gene)
-    expr3$Gene <- as.vector(expr3$Gene)
-    expr3$Stage <- factor(expr3$Stage, stages)
-    expr3$Strain <- factor(expr3$Strain, strains)
-    expr4$Strain <- factor(expr4$Strain, strains)
-    incProgress(0.1)})
+  # Update data
+  data1 <- reactive(buildExpr(expr1, input$query1, input$text1, input$geneClass01))
+  data2 <- reactive(buildExpr(expr3, input$query2, input$text2, input$geneClass02, input$conditions))
+  data3 <- reactive(buildExpr(expr2, input$query1, input$text1, input$geneClass01))
+  data4 <- reactive(buildExpr(expr4, input$query2, input$text2, input$geneClass02, input$conditions))
   
   # Render box-plots with normalized expression levels
   output$BoxPlot1 <- renderPlot({
-    expr1.subset <- expr.subset(expr1, input$text1)
-    validate(
-      need(
-        nrow(expr1.subset) > 0, msgPlot))
-    
-    plot.base <- ggplot(expr1.subset, aes(x = Gene, fill = Treatment)) + 
+    plot.data <- data1()
+    validate(need(nrow(plot.data) > 0, msgPlot))
+    plot.base <- ggplot(plot.data, aes(x = Gene, fill = Treatment)) + 
                  ggtitle('Normalized Expression Levels') + scale_fill_grey() + common.theme
     if (input$log1) {
       plot.base + geom_boxplot(aes(y = Expression)) + ylab(expression(log[2] * ' Expression Level (AU)'))}
@@ -77,12 +82,9 @@ server <- function(input, output) {
       plot.base + geom_boxplot(aes(y = 2 ^ Expression)) + ylab('Expression Level (AU)')}})
 
   output$BoxPlot2 <- renderPlot({
-    expr3.subset <- expr.subset(expr3, input$text2)
-    validate(
-      need(
-        nrow(expr3.subset) > 0, msgPlot))
-    
-    plot.base <- ggplot(expr3.subset, aes(x = Gene, fill = Stage)) + facet_wrap(~ Strain) + 
+    plot.data <- data2()
+    validate(need(nrow(plot.data) > 0, msgPlot))
+    plot.base <- ggplot(plot.data, aes(x = Gene, fill = Stage)) + facet_wrap(~ Strain) + 
                  ggtitle('Normalized Expression Levels') + common.theme
     if (input$log2) {
       plot.base + geom_boxplot(aes(y = Expression)) + ylab(expression(log[2] * ' Expression Level (AU)'))}
@@ -91,12 +93,9 @@ server <- function(input, output) {
   
   # Render bar plots with differential expression levels
   output$BarPlot1 <- renderPlot({
-    expr2.subset <- expr.subset(expr2, input$text1)
-    validate(
-      need(
-        nrow(expr2.subset) > 0, msgPlot))
-    
-    plot.base <- ggplot(expr2.subset, aes(x = Gene)) +
+    plot.data <- data3()
+    validate(need(nrow(plot.data) > 0, msgPlot))
+    plot.base <- ggplot(plot.data, aes(x = Gene)) +
                  ggtitle('Differential Expression Levels') + common.theme
     if (input$log1) {
       plot.base + geom_bar(aes(y = logFC), stat = 'identity') +
@@ -108,12 +107,9 @@ server <- function(input, output) {
                   ylab('Fold Change')}})
 
   output$BarPlot2 <- renderPlot({
-    expr4.subset <- expr.subset(expr4, input$text2)
-    validate(
-      need(
-        nrow(expr4.subset) > 0, msgPlot))
-    
-    plot.base <- ggplot(expr4.subset, aes(x = Gene)) + facet_wrap(~ Strain) +
+    plot.data <- data4()
+    validate(need(nrow(plot.data) > 0, msgPlot))
+    plot.base <- ggplot(plot.data, aes(x = Gene)) + facet_wrap(~ Strain) +
                  ggtitle('Differential Expression Levels') + common.theme
     if (input$log2) {
       plot.base + geom_bar(aes(y = logFC), stat = 'identity') +
@@ -126,45 +122,35 @@ server <- function(input, output) {
   
   # Render tables with differential expression levels
   output$Table1 <- renderTable({
-    expr2.subset <- expr.subset(expr2, input$text1)
-    validate(
-      need(
-        nrow(expr2.subset) > 0, msgTable)) 
-    expr2.subset$Gene <- sapply(expr2.subset$Gene, link.wb)
-    xtable(expr2.subset)}, digits = 3, sanitize.text.function = function(x) x)
+    table.data <- data3()
+    validate(need(nrow(table.data) > 0, msgTable))
+    table.data$Gene <- sapply(table.data$Gene, link.wb)
+    xtable(table.data)}, digits = 3, sanitize.text.function = function(x) x)
   
   output$Table2.1 <- renderTable({
-    expr4.subset <- expr.subset(expr4[expr4$Strain == 'N2', -2], input$text2)
-    validate(
-      need(
-        nrow(expr4.subset) > 0, msgTable))
-    expr4.subset$Gene <- sapply(expr4.subset$Gene, link.wb)
-    xtable(expr4.subset)}, digits = 3, sanitize.text.function = function(x) x)
+    table.data <- data4()
+    validate(need(nrow(table.data) > 0, msgTable))
+    table.data$Gene <- sapply(table.data$Gene, link.wb)
+    xtable(table.data[table.data$Strain == 'N2',-2])}, digits = 3, sanitize.text.function = function(x) x)
   
   output$Table2.2 <- renderTable({
-    expr4.subset <- expr.subset(expr4[expr4$Strain == 'daf-2', -2], input$text2)
-    validate(
-      need(
-        nrow(expr4.subset) > 0, msgTable))
-    expr4.subset$Gene <- sapply(expr4.subset$Gene, link.wb)
-    xtable(expr4.subset)}, digits = 3, sanitize.text.function = function(x) x)
-
+    table.data <- data4()
+    validate(need(nrow(table.data) > 0, msgTable))
+    table.data$Gene <- sapply(table.data$Gene, link.wb)
+    xtable(table.data[table.data$Strain == 'daf-2',-2])}, digits = 3, sanitize.text.function = function(x) x)
+  
   output$Table2.3 <- renderTable({
-    expr4.subset <- expr.subset(expr4[expr4$Strain == 'daf-2;daf-12', -2], input$text2)
-    validate(
-      need(
-        nrow(expr4.subset) > 0, msgTable))
-    expr4.subset$Gene <- sapply(expr4.subset$Gene, link.wb)
-    xtable(expr4.subset)}, digits = 3, sanitize.text.function = function(x) x)
-  
+    table.data <- data4()
+    validate(need(nrow(table.data) > 0, msgTable))
+    table.data$Gene <- sapply(table.data$Gene, link.wb)
+    xtable(table.data[table.data$Strain == 'daf-2;daf-12',-2])}, digits = 3, sanitize.text.function = function(x) x)
+
   output$Table2.4 <- renderTable({
-    expr4.subset <- expr.subset(expr4[expr4$Strain == 'daf-16', -2], input$text2)
-    validate(
-      need(
-        nrow(expr4.subset) > 0, msgTable))
-    expr4.subset$Gene <- sapply(expr4.subset$Gene, link.wb)
-    xtable(expr4.subset)}, digits = 3, sanitize.text.function = function(x) x)
-  
+    table.data <- data4()
+    validate(need(nrow(table.data) > 0, msgTable))
+    table.data$Gene <- sapply(table.data$Gene, link.wb)
+    xtable(table.data[table.data$Strain == 'daf-16',-2])}, digits = 3, sanitize.text.function = function(x) x)
+    
   output$Title2.1 <- renderText('N2')
   output$Title2.2 <- renderText('daf-2')
   output$Title2.3 <- renderText('daf-2;daf-12')
@@ -179,24 +165,39 @@ ui <- shinyUI(navbarPage(title = 'Kurzchalia Microarray Database 0.1',
       titlePanel("Desiccation"),
         sidebarLayout(  
           sidebarPanel(
-            tags$style(type="text/css", "textarea {width:100%}"),
-            tags$textarea(name = "text1", rows = 3, wrap = 'soft', placeholder = msgEntry),
+            radioButtons('query1', 'Search for', c('Genes', 'A gene class'), inline = T),
+            conditionalPanel('input.query1 == "Genes"',
+              withTags(
+                div(class = 'geneEntry', 
+                  style(type="text/css", "textarea {width:100%}"),
+                  label('Gene names:'),
+                  textarea(name = "text1", rows = 3, wrap = 'soft', placeholder = msgEntry)))),
+            conditionalPanel('input.query1 == "A gene class"',
+              selectInput('geneClass01', 'Gene classes:', choices = geneClass1, selectize = T)),
             checkboxInput("log1", label = HTML(paste('Show y-axis in log', tags$sub(2), ' scale', sep = '')))),
       
       mainPanel(
         tabsetPanel(
-          tabPanel("Expression Levels", plotOutput('BoxPlot1')),
-          tabPanel("Differential Expression", plotOutput('BarPlot1')),
-          tabPanel("Numerical Results", tableOutput('Table1'))))))),
+          tabPanel("Expression Levels", plotOutput('BoxPlot1'), icon = icon('area-chart')),
+          tabPanel("Differential Expression", plotOutput('BarPlot1'), icon = icon('bar-chart')),
+          tabPanel("Numerical Results", tableOutput('Table1'), icon = icon('table'))))))),
 
   tabPanel('Hypometabolism',   
     fluidPage(
       titlePanel("Hypometabolism"),
         sidebarLayout(  
           sidebarPanel(
-            tags$style(type="text/css", "textarea {width:100%}"),
-            tags$textarea(name = "text2", rows = 3, wrap = 'soft', placeholder = msgEntry),
-            checkboxInput("log2", label = HTML(paste('Show y-axis in log', tags$sub(2), ' scale', sep = '')))),
+            radioButtons('query2', 'Search for', c('Genes', 'A gene class'), inline = T),
+            conditionalPanel('input.query2 == "Genes"',
+              withTags(
+                div(class = 'geneEntry',
+                  style(type="text/css", "textarea {width:100%}"),
+                  label('Gene names:'),
+                  textarea(name = "text2", rows = 3, wrap = 'soft', placeholder = msgEntry)))),
+            conditionalPanel('input.query2 == "A gene class"',
+              selectInput('geneClass02', 'Gene classes:', choices = geneClass2, selectize = T)),
+            checkboxInput(inputId = "log2", label = HTML(paste('Show y-axis in log', tags$sub(2), ' scale', sep = ''))),
+            checkboxGroupInput(inputId = 'conditions', label = 'Strains', choices = levels(expr3$Strain), selected = levels(expr3$Strain))),
                
       mainPanel(
         tabsetPanel(
